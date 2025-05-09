a5db7e67ef34a21a3c00c128944d93dd30c5b60c
Yves LAMBERT <yves.lambert@fms-ea.com>
2025-05-09 15:50:48 +0200

    début de la troisième partie

--- rp_flask_api/models.py
+++ rp_flask_api/models.py
@@ -3,6 +3,15 @@
 from datetime import datetime
 from config import db, ma
 
+class Task(db.Model):
+    __tablename__ = "task"
+    id = db.Column(db.Integer, primary_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey("person.id"))
+    content = db.Column(db.String, nullable=False)
+    timestamp = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+    
 class Person(db.Model):
     __tablename__ = "person"
     id = db.Column(db.Integer, primary_key=True)
@@ -10,6 +19,12 @@ class Person(db.Model):
     fname = db.Column(db.String(32))
     timestamp = db.Column(
         db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+    tasks = db.relationship("Task", 
+                            backref="person", 
+                            cascade="all, delete, delete-orphan",
+                            single_parent=True,
+                            order_by="Task.timestamp)"
     )
 class PersonSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
